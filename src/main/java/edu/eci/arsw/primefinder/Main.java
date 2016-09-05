c77970cf606ca6ba78c0b97c5457202b68697b93
Manuel Felipe S.R <manuelfelip-96@hotmail.com>
2016-09-05 09:10:33 -0500

    First Commit

--- src/main/java/edu/eci/arsw/primefinder/Main.java
+++ src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,60 @@
 package edu.eci.arsw.primefinder;
 
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
-	}
-	
+    static PrimeFinderThread pft, pft2, pft3;
+
+    public static void EntertoStart() throws IOException {
+
+        //Scanner teclado = new Scanner(System.in);
+        boolean validator = true;
+        //char tecla = (char)System.in.read();
+        System.out.println("|––––––––––––––––––––––––––––––––|\n"
+                + "| Han pasado 5 Segundos ,Digite  |\n"
+                + "| ENTER para continuar           |\n"
+                + "|________________________________|\n");
+        while (validator) {
+
+            if (System.in.read() == KeyEvent.VK_ENTER) {
+
+                PrimeFinderThread.control = true;
+                validator = false;
+
+            } else {
+                System.out.println("No puedo continuar si no digita ENTER");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        pft = new PrimeFinderThread(0, 10000000);
+        pft2 = new PrimeFinderThread(10000000, 20000000);
+        pft3 = new PrimeFinderThread(20000000, 30000000);
+
+        pft.start();
+        pft2.start();
+        pft3.start();
+        Thread.sleep(4000);
+        pft.setControl(false);
+        pft2.setControl(false);
+        pft3.setControl(false);
+         EntertoStart();
+        /*Thread.sleep(5000);*/
+
+       
+
+        synchronized (PrimeFinderThread.key) {
+            pft.getKey().notify();
+            pft2.getKey().notify();
+            pft3.getKey().notify();
+        }
+
+    }
+
 }
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -2,45 +2,85 @@ package edu.eci.arsw.primefinder;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrimeFinderThread extends Thread{
 
 	
-	int a,b;
-	
+	int a,b,c;
+	static Object key;
 	private List<Integer> primes=new LinkedList<Integer>();
-	
+       static  boolean control = true;
 	public PrimeFinderThread(int a, int b) {
 		super();
 		this.a = a;
 		this.b = b;
+                key = new Object();
+                System.out.println("|_____________________________________|\n"
+                                +  "| Programa para validar si los numeros|\n"
+                                 + "| son primos de manera concurrente    |\n"
+                                 + "|_____________________________________|\n");
+                
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
-	}
+    public Object getKey() {
+        return key;
+    }
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
-	
-	
-	
+    public void setKey(Object key) {
+        this.key = key;
+    }
+
+    public void run() {
+        System.out.println("|____________________________________|\n");
+        for (int i = a; i <= b; i++) {
+
+            if (control) {
+                if (isPrime(i)) {
+                    primes.add(i);
+                    System.out.println("|Numero  ----> "+i + "Es primo|\n");
+
+                } 
+            }else{
+                try {
+            /*        System.out.println("Estoy con hilos Sincronizados");*/
+                    synchronized(key){
+                    key.wait();
+                    }
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(PrimeFinderThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        System.out.println("|____________________________________|\n");
+
+    }
+
+    public boolean isControl() {
+        return control;
+    }
+
+    public void setControl(boolean control) {
+        this.control = control;
+    }
+
+    boolean isPrime(int n) {
+      
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+          c++;
+        return true;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
 }
